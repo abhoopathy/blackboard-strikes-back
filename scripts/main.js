1d859dadb1304f5f64b9160c3c65448d083e3559
Aneesh <aneesh.bhoopathy@gmail.com>
2012-10-17 18:10:24 -0400

    crushed flexible left col issue

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,37 @@
-$(document).ready(function() {
-    $('.class-list-item').click(function () {
-        $ul = $(this).find('.class-sublist');
-        $ul.slideToggle(150);
-        console.log( $('.class-list-container').height() );
+// http://dl.dropbox.com/u/5354845/wboard/index.html
 
-//        $('.task-list-item > .task-list-item-tag:not(.color1)').each(function () {
-//            $(this).closest('.task-list-item').fadeOut();
-//        });
+$(document).ready(function() {
+
+    var $classes = $('.class-list-container');
+    var $tasks = $('.task-list-container');
+    var $sidecol = $('.side-col');
+
+    $tasks.height($sidecol.height() - $classes.height());
+
+    $('.class-list-item').click(function () {
+
+        var $ul = $(this).find('.class-sublist');
+        var class_id = $(this).attr('data-classid');
+        console.log(class_id);
+        
+
+        $ul.toggle();
+        var new_height = $sidecol.height() - $classes.height();
+        $ul.toggle();
+
+        $ul.animate( {height: 'toggle'}, {duration: 150, queue: false} );
+        $tasks.animate( {height: new_height}, {duration: 150, queue: false} );
+
+        if ($(this).hasClass('.expanded')) {
+            $(this).removeClass('.expanded');
+            $('.task-list-item').fadeIn();
+        } else {
+            $(this).addClass('.expanded');
+            $('.task-list-item > .task-list-item-tag:not(.color'+class_id+')').each(function () {
+                $(this).closest('.task-list-item').fadeOut();
+            });
+        }
+
     });
 
 
