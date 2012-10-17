a295dc46c0953997b407a98bd1142fd07098f1ca
Aneesh <aneesh.bhoopathy@gmail.com>
2012-10-17 15:51:44 -0400

    initial

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('.class-list-item').click(function () {
+        $ul = $(this).find('.class-sublist');
+        $ul.slideToggle(150);
+        console.log( $('.class-list-container').height() );
+
+        $('.task-list-item > .task-list-item-tag:not(.color1)').each(function () {
+            $(this).closest('.task-list-item').fadeOut();
+        });
+    });
+
+
+});
