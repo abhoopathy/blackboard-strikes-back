9ca1046ead21b3846c7edafe6a1cac1b2410be13
Aneesh <aneesh.bhoopathy@gmail.com>
2012-10-17 16:11:14 -0400

    calendar stuff

--- scripts/main.js
+++ scripts/main.js
@@ -4,9 +4,9 @@ $(document).ready(function() {
         $ul.slideToggle(150);
         console.log( $('.class-list-container').height() );
 
-        $('.task-list-item > .task-list-item-tag:not(.color1)').each(function () {
-            $(this).closest('.task-list-item').fadeOut();
-        });
+//        $('.task-list-item > .task-list-item-tag:not(.color1)').each(function () {
+//            $(this).closest('.task-list-item').fadeOut();
+//        });
     });
 
 
