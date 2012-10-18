b7fe9e974006d18a0fab87fb9ffb0be5db76b6b4
Aneesh <aneesh.bhoopathy@gmail.com>
2012-10-17 23:41:02 -0400

    started backbone work, need to commit to switch to master

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,38 +1,27 @@
-// http://dl.dropbox.com/u/5354845/wboard/index.html
+//Define shortcut aliases
 
-$(document).ready(function() {
+require.config({
+    paths: {
+        jquery: 'libs/jquery/jquery-1.8.0.min',
+        underscore: 'libs/underscore/underscore-min',
+        backbone: 'libs/backbone/backbone-min'
+    },
 
-    var $classes = $('.class-list-container');
-    var $tasks = $('.task-list-container');
-    var $sidecol = $('.side-col');
-
-    $tasks.height($sidecol.height() - $classes.height());
-
-    $('.class-list-item').click(function () {
-
-        var $ul = $(this).find('.class-sublist');
-        var class_id = $(this).attr('data-classid');
-        console.log(class_id);
-        
-
-        $ul.toggle();
-        var new_height = $sidecol.height() - $classes.height();
-        $ul.toggle();
-
-        $ul.animate( {height: 'toggle'}, {duration: 150, queue: false} );
-        $tasks.animate( {height: new_height}, {duration: 150, queue: false} );
-
-        if ($(this).hasClass('.expanded')) {
-            $(this).removeClass('.expanded');
-            $('.task-list-item').fadeIn();
-        } else {
-            $(this).addClass('.expanded');
-            $('.task-list-item > .task-list-item-tag:not(.color'+class_id+')').each(function () {
-                $(this).closest('.task-list-item').fadeOut();
-            });
+    shim: {
+        backbone: {
+            deps: ['underscore', 'jquery'],
+            exports: 'Backbone'
+        },
+        underscore: {
+            exports: '_'
+        },
+        jquery: {
+            exports: '$'
         }
+    }
 
-    });
+});
 
-
+require(['cs!app'], function(App) {
+    App.initialize();
 });
